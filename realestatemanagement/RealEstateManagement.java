4a2baa69d87e3c101810348363516505d30b2b62
mkmaeda <118786287+mkmaeda@users.noreply.github.com>
2026-03-03 22:36:34 +0800

    Libraries

---
+++ realestatemanagement/RealEstateManagement.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.realestatemanagement;
+
+/**
+ *
+ * @author Kouji
+ */
+public class RealEstateManagement {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
