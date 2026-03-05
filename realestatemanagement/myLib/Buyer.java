5da9e024cd97a76cb51f176a67d2ce781a6f0908
mkmaeda <118786287+mkmaeda@users.noreply.github.com>
2026-03-05 16:20:51 +0800

    Buyer.java

--- realestatemanagement/myLib/Buyer.java
+++ realestatemanagement/myLib/Buyer.java
@@ -1,13 +1,19 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.realestatemanagement.myLib;
 
-/**
- *
- * @author Kouji
- */
 public class Buyer {
+    private String name;
+    private String contact;
     
+    public Buyer(String name, String contact) {
+        this.name = name;
+        this.contact = contact;
+    }
+    
+    public String getName() {return name; }
+    public String getContact() {return contact; }
+    
+    @Override
+    public String toString() {
+        return name + " (" + contact + ")";
+    }          
 }
