5a8df7268c4985688d7122aeb1c2d11de3ccb45d
mkmaeda <118786287+mkmaeda@users.noreply.github.com>
2026-03-05 16:21:29 +0800

    Transaction.java

--- realestatemanagement/myLib/Transaction.java
+++ realestatemanagement/myLib/Transaction.java
@@ -1,13 +1,31 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.realestatemanagement.myLib;
 
-/**
- *
- * @author Kouji
- */
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Transaction {
+    private Lot lot;
+    private Buyer buyer;
+    private String action; //if reserved or sold
+    private String timestamp;
+    
+    public Transaction(Lot lot, Buyer buyer, String action) {
+        this.lot = lot;
+        this.buyer = buyer;
+        this.action = action;
+        this.timestamp = LocalDateTime.now()
+                .format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+    }
+    
+    public Lot getLot() { return lot;}
+    public Buyer getBuyer() { return buyer;}
+    public String getAction() { return action;}
+    public String getTimeStamp() { return timestamp;}
+    
+    @Override
+    public String toString() {
+        return String.format("[%s] %s: Block %d, Lot %d -> %s",
+                timestamp, action, lot.getBlockNumber(), lot.getLotNumber(), buyer.getName());
+    }
     
 }
